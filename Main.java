f0d0c8331532dd3557eafe3b307aa3873cb95e9d
sarahcarroll7 <149721303+sarahcarroll7@users.noreply.github.com>
2023-11-09 17:06:09 -0500

    Sarah added supplier array
    I added in the supplier array and then created 3 different suppliers

--- Main.java
+++ Main.java
@@ -6,10 +6,40 @@ import java.util.HashSet;
 
 public class Main {
 	public static void main(String [] args){
-    
-	Scanner scnr = new Scanner(System.in);
+		//Supplier class
+		public class Supplier {
+			private String name;
+			private String location;
+			private String contact;
+			
+			public Supplier(String name, String location, String contact) {
+				this.name = name;
+				this.location = location;
+				this.contact = contact;	
+				}
+			public String getname() {
+				return name;
+			}
+			public String getLocation() {
+				return location;
+			}
+			public String getContact() {
+				return contact;
+			}
+		}
+		        
+		for (Supplier supplier : suppliers) {
+			System.out.println("Supplier: " + supplier.getName()
+			+ "Location: " + supplier.getLocation() +
+			"Contact: " + supplier.getContact());
+		}
+		
+		Supplier[] suppliers = {
+				new Supplier("Flirt", "Georgia", "404-982-7654");
+				new Supplier("Beauty Babes", "Florida", "901-879-0342");
+				new Supplier("Party Girl Prom", "Alabama", "860-123-6754");
+		}
+
 	
-		
-		
 	}
 }
