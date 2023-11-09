9337ec5a84f668728e189d981655e65572477a18
Jonah-Kilpi <jonahkilpi@gmail.com>
2023-11-09 17:06:05 -0500

    jonah Added Array List
    Featured Products names, price, and company

--- Main.java
+++ Main.java
@@ -7,7 +7,51 @@ import java.util.HashSet;
 public class Main {
 	public static void main(String [] args){
     
-		
+		 Products
+		        FeaturedProduct[] featuredProducts = {
+				                new FeaturedProduct("Don't Tell Mom", "Flirt", 799.99),
+				                new FeaturedProduct("Walk It Like You Talk It", "Beauty Babes", 550.99),
+				                new FeaturedProduct("Spicy and Sexy", "Party Girl Prom", 699.99)
+				        };
+
+				        for (Supplier supplier : suppliers) {
+				            System.out.println("Supplier: " + supplier.getName() +
+				                    ", Location: " + supplier.getLocation() +
+				                    ", Contact: " + supplier.getContact());
+				        }
+
+				        for (FeaturedProduct product : featuredProducts) {
+				            System.out.println("Product: " + product.getName() +
+				                    ", Supplier: " + product.getSupplier() +
+				                    ", Price: $" + product.getPrice());
+				        }
+				    }
+				}
+
+				
+				class FeaturedProduct {
+				    private String name;
+				    private String supplier;
+				    private double price;
+
+				    public FeaturedProduct(String name, String supplier, double price) {
+				        this.name = name;
+				        this.supplier = supplier;
+				        this.price = price;
+				    }
+
+				    public String getName() {
+				        return name;
+				    }
+
+				    public String getSupplier() {
+				        return supplier;
+				    }
+
+				    public double getPrice() {
+				        return price;
+				    }
+				}
 		
 		
 	}
