4da9f6cd586d01a87d7c46a93b1315ed37e697a2
zm2231 <merchantzains@gmail.com>
2023-11-14 21:37:43 -0500

    Setup the Boutique.java file

--- Boutique.java
+++ Boutique.java
@@ -1,3 +1,44 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+
 public class Boutique {
-	
+    private HashSet<Product> products;
+    private ArrayList<FeaturedProduct> featuredProducts;
+    private Supplier[] suppliers;
+
+    public Boutique() {
+        products = new HashSet<>();
+        featuredProducts = new ArrayList<>();
+        initializeSuppliers();
+        initializeProducts();
+    }
+
+    private void initializeSuppliers() {
+        // to-do, copy over suppliers from Main.java
+    }
+
+    private void initializeProducts() {
+        // to-do, copy over products from Main.java
+    }
+
+    public  void addFeaturedProduct(String productName) {
+        for (Product product : products) {
+            if (product.getName().equalsIgnoreCase(productName)) {
+                featuredProducts.add(new FeaturedProduct(product.getName(), product.getSupplier(), product.getPrice()));
+                break;
+            }
+        }
+    }
+
+    public HashSet<Product> getProducts() {
+        return products;
+    }
+
+    public ArrayList<FeaturedProduct> getFeaturedProducts() {
+        return featuredProducts;
+    }
+
+    public Supplier[] getSuppliers() {
+        return suppliers;
+    }
 }
