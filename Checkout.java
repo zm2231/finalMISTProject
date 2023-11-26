c434c826ea3f7c227c3ca945fc61beb477976c80
zm2231 <merchantzains@gmail.com>
2023-11-26 17:28:31 -0500

    Reverted part of the last commit

--- Checkout.java
+++ Checkout.java
@@ -1,78 +1,76 @@
- import java.util.HashSet;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.HashMap;
 public class Checkout {
 
-private HashSet<String> Checkout = new HashSet<String>();
-private static HashMap<String,Double> TodayProducts = new HashMap<String,Double>();
+	private HashSet<String> Checkout = new HashSet<String>();
+	private static HashMap<String,Double> TodayProducts = new HashMap<String, Double>();
 
-private double totalCost;
-private User user;
-private String memberID;
+	private int i;
+	private double totalCost;
+	private String MemberID;
+	private boolean IsMember;
 
-
-
-public Checkout(User user) {
-	this.user = user;
-	this.cartItems = new HashSet<>();
-	this.itemPrices = new HashMap<>();
-}
-
-public void addItemsToCart (Product product) {
-	checkoutItems.add(product.getName().toLowerCase());
-	todayProducts.put(product.getName(), product.applyDiscount(user));
+	public Checkout(User user) {
+		this.user = user;
+		this.cartItems = new HashSet<>();
+		this.itemPrices = new HashMap<>();
 	}
-public static void Selections(String id, Double price) {
-	todayProducts.put(id, price);
-}
+
+	public void addItemsToCart (Product product) {
+		Checkout.add(product.getName().toLowerCase());
+		TodayProducts.put(product.getName(), product.applyDiscount(user));
+	}
+
+	public static void Selections(String id, Double price) {
+		TodayProducts.put(id, price);
+	}
 
 	public void addCart(Scanner scnr) {
 		String addition = scnr.nextLine();
 		String key = addition.toLowerCase();
 		String check = " ";
-		
-	while(!check.equals("done")) {
 
-		for(String name : todayProducts.keySet()) {
-			String lowerCaseProducts = name.toLowerCase();
-			if(lowerCaseProducts.equals(key)) {
-				Checkout.add(key);
-				System.out.println("Sucuessfully Added: " + name);
+		while(!check.equals("done")) {
+
+			for(String name : TodayProducts.keySet()) {
+				String LowerCaseProducts = name.toLowerCase();
+				if(LowerCaseProducts.equals(key)) {
+					Checkout.add(key);
+					System.out.println("Sucuessfully Added: " + name);
+				}
+
 			}
+
+
 			key = scnr.nextLine();
 			check = key.toLowerCase();
 		}
-		
-		
-		key = scnr.nextLine();
-		check = key.toLowerCase();
-		}
 	}
-	
+
 	public void setMemberShip(String ID) {
-		memberID = ID;
+		MemberID = ID;
 	}
-	
-	public void isMember(Boolean set) {
-		isMember = set;
+
+	public void IsMember(Boolean Set) {
+		IsMember = Set;
 	}
-	
+
 	public void printCart() {
-		for(String products : todayProducts.keySet()) {
-			
-			String Checker = products.toLowerCase();
-			
+		for(String Products : TodayProducts.keySet()) {
+
+			String Checker = Products.toLowerCase();
+
 			if(Checkout.contains(Checker)) {
-				System.out.println(Products + ": " + todayProducts.get(products));
-				totalCost += todayProducts.get(products);
+				System.out.println(Products + ": " + TodayProducts.get(Products));
+				totalCost += TodayProducts.get(Products);
+			}
 		}
-	}
-		if(isMember = true) {
+		if(IsMember = true) {
 			totalCost = totalCost * (1-.10);
 		}
 		System.out.println("Total Cost: " + totalCost);
 	}
-	
-}
 
+}
 
