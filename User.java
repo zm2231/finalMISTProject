cd639ad8f37d49fa2b6a66cfca85c2d714b102a7
zm2231 <merchantzains@gmail.com>
2023-11-27 18:05:15 -0500

    MembershipID

--- User.java
+++ User.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 public class User {
     private String username;
     private String password;
@@ -7,6 +9,8 @@ public class User {
     private int birthDay;
     private boolean isDOBSet;
     private boolean isMember;
+    private String membershipID;
+    private static ArrayList<String> allMembershipIDs = new ArrayList<String>();
 
     public User(String username, String password, String email, boolean isMember) {
         this.username = username;
@@ -14,6 +18,9 @@ public class User {
         this.email = email;
         this.isDOBSet = false;
         this.isMember = isMember;
+        if (isMember) {
+            addMembership();
+        }
     }
 
     public User(String username, String password, String email, boolean isMember, int birthYear, int birthMonth, int birthDay) {
@@ -25,22 +32,16 @@ public class User {
         this.birthDay = birthDay;
         this.isDOBSet = true;
         this.isMember = isMember;
-    }
-
-    public boolean isBirthday(int month, int day) {
-        return isDOBSet && this.birthMonth == month && this.birthDay == day;
+        if (isMember) {
+            addMembership();
+        }
     }
 
     public double applyDiscount(double price) {
         double discount = 0.0;
-        int currentMonth = 5;
-        int currentDay = 15;
 
         if (this.isMember) {
             discount += 0.10;
-        }
-        if (isBirthday(currentMonth, currentDay)) {
-            discount += 0.05;
         }
         return price * (1 - discount);
     }
@@ -51,11 +52,38 @@ public class User {
 
     public void addMembership() {
         this.isMember = true;
+        this.membershipID = generateMembershipID();
+        allMembershipIDs.add(this.membershipID);
+        System.out.println("Your membership ID is " + this.membershipID);
     }
 
     public void removeMembership() {
         this.isMember = false;
+        allMembershipIDs.remove(this.membershipID);
+        this.membershipID = null;
     }
 
+    private static String generateMembershipID() {
+        Random random = new Random();
+        int number = random.nextInt(9000) + 1000;
+        String generatedID = String.valueOf(number);
+        while (allMembershipIDs.contains(generatedID)) {
+            number = random.nextInt(9000) + 1000;
+            generatedID = String.valueOf(number);
+        }
+        return generatedID;
+    }
 
+    public static boolean isMembershipValid(String membershipID) {
+        return allMembershipIDs.contains(membershipID);
+    }
+
+    public void printMembershipID() {
+        if (this.isMember && this.membershipID != null) {
+            System.out.println("Your membership ID is " + this.membershipID);
+        }
+        else {
+            System.out.println("You are not a member.");
+        }
+    }
 }
